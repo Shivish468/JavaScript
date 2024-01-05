1bc58a6fe1a75cf87680804e42e2a76ba7563b37
Shivish468 <shivamaryan468@gmail.com>
2024-01-06 00:33:31 +0530

    function call in JS

--- 10_classes_and_oop/call.js
+++ 10_classes_and_oop/call.js
@@ -11,5 +11,5 @@ function createUser(username, email, password){
     this.password = password
 }
 
-const chai = new createUser("chai", "chai@fb.com", "123")
-console.log(chai);
+const code = new createUser("code", "code@fb.com", "123")
+console.log(code);
